d66f4979a740dd399b6b03f8f7b375e9dcbcd1f5
Arthur Valério <31370475+arthwrvl@users.noreply.github.com>
2023-01-03 12:58:00 -0300

    Add communication bridge

---
+++ Assets/PluginHelper.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PluginHelper : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textResult;
+
+    [DllImport("__Internal")]
+    private static extern int _addTwoNumberInIOS(int a, int b);
+
+    void Start()
+    {
+        AddTwoNumber();
+    }
+
+    public void AddTwoNumber()
+    {
+        int result = _addTwoNumberInIOS(10, 5);
+        textResult.text = "10 + 5  is : " + result;
+    }
+}
